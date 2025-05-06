6c337088b8b357c439c427fd26516a50d32d6231
denisgaker <denisgaker@gmail.com>
2025-05-06 16:34:20 +0300

    ИСоздание и изменение контакта

--- index.php
+++ index.php
@@ -12,11 +12,11 @@ get_req();
 
 function get_req()
 {
-//    $req = $_REQUEST;
-//    $time = time();
-//    file_put_contents(__DIR__."/tmp/req_{$time}_.json", json_encode($req));
+    $req = $_REQUEST;
+    $time = time();
+    file_put_contents(__DIR__."/tmp/req_{$time}_.json", json_encode($req));
 //die();
-    $req = json_decode(file_get_contents(__DIR__."/tmp/req_1746536449_.json"), 1);
+//    $req = json_decode(file_get_contents(__DIR__."/tmp/req_1746537954_.json"), 1);
 //    vd($req, '$req', 1);
 
     switch (true)
@@ -29,9 +29,71 @@ function get_req()
             lead_update($req);
             break;
 
+        case isset($req["contacts"]["add"]):
+            contacts_add($req);
+            break;
+
+        case isset($req["contacts"]["update"]):
+            update_add($req);
+            break;
+
         default:
             break;
     }
+}
+
+function update_add($req)
+{
+    $contact = $req["contacts"]["update"][0];
+
+    $data = [[
+        "filter" => [
+            "entity" => ["contact"],
+            "entity_id" => [$contact["id"]],
+        ]
+    ]];
+
+    $news = "";
+
+    $event = get_request("events", $data);
+
+    foreach ($event["_embedded"]["events"] as $e)
+    {
+        if ((int)$e["created_at"] === (int)$contact["last_modified"])
+        {
+            vd($e["value_after"], 'e', 0);
+
+            if (isset($e["value_after"][0]["sale_field_value"]))
+                $news .= "Бюджет = ".$e["value_after"][0]["sale_field_value"]["sale"]."\n";
+
+            if (isset($e["value_after"][0]["custom_field_value"]))
+            {
+                $field = get_request("contacts/custom_fields/".$e["value_after"][0]["custom_field_value"]["field_id"])["name"];
+                $news .= $field." = ".$e["value_after"][0]["custom_field_value"]["text"]."\n";
+            }
+        }
+    }
+
+    $title = $contact["name"];
+    $id = $contact["id"];
+    $responsible_user_id = $contact["responsible_user_id"];
+    $date_update = date("d-m-Y H:i:s", (int)$contact["updated_at"]);
+
+    $user = get_request("users/{$responsible_user_id}");
+
+    $responsible_user_name = (isset($user["name"])) ? "Ответственный пользователь: ".$user["name"] : "Не удалось поределить ответственного пользователя";
+
+    $text = "Изменен Контакт\nНазвание: {$title}\n{$responsible_user_name}\nВремя создания: {$date_update}\n\nНовые поля\n{$news}";
+
+    $data = [[
+        "entity_id" => (int)$id,
+        "note_type" => "common",
+        "responsible_user_id" => (int)$responsible_user_id,
+        "params" => [
+            "text" => $text
+        ]
+    ]];
+    $add_note = post_request("contacts/notes", $data);
 }
 
 function lead_update($req)
@@ -88,6 +150,31 @@ function lead_update($req)
     $add_note = post_request("leads/notes", $data);
 }
 
+
+function contacts_add($req)
+{
+    $name = $req["contacts"]["add"][0]["name"];
+    $id = $req["contacts"]["add"][0]["id"];
+    $responsible_user_id = $req["contacts"]["add"][0]["responsible_user_id"];
+    $date_create = date("d-m-Y H:i:s", (int)$req["contacts"]["add"][0]["date_create"]);
+
+    $user = get_request("users/{$responsible_user_id}");
+
+    $responsible_user_name = (isset($user["name"])) ? "Ответственный пользователь: ".$user["name"] : "Не удалось поределить ответственного пользователя";
+
+    $text = "Добавлен контакт\nНазвание: {$name}\n{$responsible_user_name}\nВремя создания: {$date_create}\n";
+
+    $data = [[
+        "entity_id" => (int)$id,
+        "note_type" => "common",
+        "responsible_user_id" => (int)$responsible_user_id,
+        "params" => [
+            "text" => $text
+        ]
+    ]];
+    $add_note = post_request("contacts/notes", $data);
+    vd($add_note, '$add_note', 0);
+}
 
 function lead_add($req)
 {
